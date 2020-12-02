8130b56b15d2159fab700200fbc60683523e5538
suyogvhaval <52542431+suyogvhaval@users.noreply.github.com>
2020-12-02 10:22:38 +0530

    Add files via upload

---
+++ ConsoleApplication2OOP/ConsoleApplication2OOP/BaseClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2OOP
+{
+    class BaseClass
+    {
+        protected virtual void m1()
+        {
+            Console.WriteLine("In Base Class");
+        }
+    }
+}
---
+++ ConsoleApplication2OOP/ConsoleApplication2OOP/DerivedClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2OOP
+{
+    class DerivedClass: BaseClass
+    {
+        public override void m1()
+        {
+            Console.WriteLine("In Derived Class");
+        }
+        
+    }
+}
