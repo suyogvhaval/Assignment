d80428ff3c0c8a9121a2d3a027d195a904090398
suyogvhaval <52542431+suyogvhaval@users.noreply.github.com>
2020-12-02 10:23:40 +0530

    Add files via upload

---
+++ Assignment3Thread/Assignment3Thread/Bank.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3Thread
+{
+    class Bank
+    {
+        double NetBalance = 50000;
+        double transamount;
+
+
+
+
+        public void deposit()
+        {
+            NetBalance = NetBalance + transamount;
+            Console.WriteLine(NetBalance);
+        }
+
+
+
+
+        public void withdrawal()
+        {
+            NetBalance = NetBalance - transamount;
+            Console.WriteLine(NetBalance);
+        }
+
+
+
+
+        public Bank(double transamount)
+        {
+            this.transamount = transamount;
+        }
+        public double Netbalance
+        {
+            get
+            {
+                return NetBalance;
+            }
+            set
+            {
+                NetBalance = value;
+            }
+        }
+    }
+}
