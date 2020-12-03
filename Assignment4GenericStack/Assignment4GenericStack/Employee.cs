b92b594a576a1304b3517d45cc22f10d5213ed9e
suyogvhaval <52542431+suyogvhaval@users.noreply.github.com>
2020-12-03 10:26:38 +0530

    Add files via upload

---
+++ Assignment4GenericStack/Assignment4GenericStack/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4GenericStack
+{
+    class Employee
+    {
+        public int empno;
+        public string empname;
+        public int salary;
+    }
+}
