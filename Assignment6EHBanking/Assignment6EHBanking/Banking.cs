8077cbbee675b9556feb4ccb22532c469890a7a9
suyogvhaval <52542431+suyogvhaval@users.noreply.github.com>
2020-12-04 11:13:10 +0530

    Add files via upload

---
+++ Assignment6EHBanking/Assignment6EHBanking/Banking.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment6EHBanking
+{
+   public class Banking
+    {
+
+        public int Netbalance
+        {
+            get;
+            set;
+        }
+         public void OverBalance(int amount)
+        {
+            double tax = amount * 0.18;
+            MessageBox.Show("Your Tax Amount:" + tax);
+        }
+        public void UnderBalance(int amount)
+        {
+            MessageBox.Show("Your balance is Below 5000. Please maintain minimum balance in your account");
+        }
+    }
+}
---
+++ Assignment6EHBanking/Assignment6EHBanking/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment6EHBanking
+{
+    public partial class Form1 : Form
+    {
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Banking b = new Banking();
+            b.Netbalance = Convert.ToInt32(textBox1.Text);
+
+            if(b.Netbalance >= 100000)
+            {
+                b.OverBalance(b.Netbalance);
+            }
+            else if(b.Netbalance < 5000)
+            {
+                b.UnderBalance(b.Netbalance);
+            }
+            else
+            {
+                MessageBox.Show("Congrasulations you dont have to pay any tax and minimum balance is maintained for your account ");
+            }
+        }
+    }
+}
